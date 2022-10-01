c53eb45c2eca9d1300b62a629e85a1fee7a70f1d
JAM <electrojam@hotmail.com>
2022-10-01 18:38:03 -0500

    Chatlist Matched 1st part

--- screens/MatchedScreen.js
+++ screens/MatchedScreen.js
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import tw from 'twrnc'
 
 const MatchedScreen = () => {
+  const navigation = useNavigation()
+  const { params } = useRoute()
+
+  const { loggedInProfile, userSwiped } = params
+
   return (
-    <View>
-      <Text>MatchedScreen</Text>
+    <View style={[tw`h-full bg-red-500 pt-20`, { opacity: 0.89 }]}>
+      <View style={tw`justify-center px-10 pt-20`}>
+        <Image 
+          style={tw`h-20 w-full`} 
+          source={{ uri: "https://links.papareact.com/mg9" }}
+        />
+      </View>
+
+      <Text style={tw`text-white text-center mt-5`}>
+        Tú y {userSwiped.displayName} se gustan
+      </Text>
+
+      <View style={tw`flex-row justify-evenly mt-5`}>
+        <Image 
+          style={tw`h-32 w-32 rounded-full`}
+          source={{ 
+            uri: loggedInProfile.photoURL,
+           }}
+        />
+        <Image 
+          style={tw`h-32 w-32 rounded-full`}
+          source={{ 
+            uri: userSwiped.photoURL,
+           }}
+        />
+      </View>
+
+      <TouchableOpacity 
+        style={tw`bg-white m-5 px-10 py-6 rounded-full mt-20`}
+        onPress={() => {
+          navigation.goBack()
+          navigation.navigate("Chat")
+        }}
+      >
+        <Text style={tw`text-center`}>Envia un mensaje</Text>
+      </TouchableOpacity>
     </View>
   )
 }
